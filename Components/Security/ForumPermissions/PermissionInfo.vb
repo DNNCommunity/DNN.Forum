d7c24656b4d0b21c1b9b0be7d17168dd4099faf3
SND\cpaterra_cp <SND\cpaterra_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2010-12-29 00:35:09 +0000

    Changed copyright to 2011 (since any future release will be in 2011).
    git-svn-id: https://dnnforum.svn.codeplex.com/svn@56589 142e9c35-602f-4880-9f37-ec48a24daa6a

--- Components/Security/ForumPermissions/PermissionInfo.vb
+++ Components/Security/ForumPermissions/PermissionInfo.vb
@@ -1,6 +1,6 @@
 '
 ' DotNetNuke® - http://www.dotnetnuke.com
-' Copyright (c) 2002-2010
+' Copyright (c) 2002-2011
 ' by DotNetNuke Corporation
 '
 ' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
