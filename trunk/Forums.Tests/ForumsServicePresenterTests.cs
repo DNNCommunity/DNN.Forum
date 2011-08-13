f53164ae349eddfd13e12c9c6b46f0fe53c09525
SND\irobinson_cp <SND\irobinson_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-08-13 02:32:14 +0000

    Added a tests project with some pretty contrived (but hopefully helpful) sample tests for the web service presenter.
    git-svn-id: https://dnnforum.svn.codeplex.com/svn@62540 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ trunk/Forums.Tests/ForumsServicePresenterTests.cs
@@ -0,0 +1,73 @@
+﻿namespace DotNetNuke.Modules.Forums.Tests
+{
+    using System.Collections.Generic;
+    using Components.Controllers;
+    using Components.Entities;
+    using MbUnit.Framework;
+    using Moq;
+    using Services;
+
+    [TestFixture]
+    public class ForumsServicePresenterTests
+    {
+        private Mock<IForumsServiceView> view;
+        private ForumsServicePresenter presenter;
+        private Mock<IForumsController> forumsController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            view = new Mock<IForumsServiceView>();
+            forumsController = new Mock<IForumsController>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        // [MethodName]_[StateUnderTest]_[ExpectedBehavior]
+
+        [Test]
+        public void GetForumEvent_ReturnsForum_WhenForumIdIsValid()
+        {
+            // Arrange
+            var infos = new List<ForumInfo>();
+            var forumInfo = new ForumInfo { Active = true, Description = "Awesome", ForumId = 1 };
+            infos.Add(forumInfo);
+            infos.Add(new ForumInfo { Active = true, Description = "What?", ForumId = 2 });
+            forumsController.Setup(x => x.GetForum(1)).Returns(forumInfo);
+            presenter = new ForumsServicePresenter(view.Object, forumsController.Object);
+
+            // Act
+            var getForumCalledEventArgs = new GetForumCalledEventArgs(1);
+            view.Raise(x => x.ForumGetCalled += null, getForumCalledEventArgs);
+            presenter.ReleaseView();
+            
+            // Assert
+            Assert.AreEqual(getForumCalledEventArgs.Result.ForumId, 1);
+        }
+
+        [Test]
+        public void GetForumEvent_ReturnsNull_WhenForumIdIsInvalid()
+        {
+            // Arrange
+            var infos = new List<ForumInfo>();
+            var forumInfo = new ForumInfo { Active = true, Description = "Awesome", ForumId = 1 };
+            infos.Add(forumInfo);
+            infos.Add(new ForumInfo { Active = true, Description = "What?", ForumId = 2 });
+            forumsController.Setup(x => x.GetForum(1)).Returns(forumInfo);
+            presenter = new ForumsServicePresenter(view.Object, forumsController.Object);
+
+            // Act
+            var getForumCalledEventArgs = new GetForumCalledEventArgs(6);
+            view.Raise(x => x.ForumGetCalled += null, getForumCalledEventArgs);
+            presenter.ReleaseView();
+
+            // Assert
+            Assert.IsNull(getForumCalledEventArgs.Result);
+        }
+
+    }
+}
---
+++ trunk/Forums.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetNuke.Modules.Forums.Tests")]
+[assembly: AssemblyDescription("Open Source Web Application Framework's Discussion Forum Module")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DotNetNuke Corporation")]
+[assembly: AssemblyProduct("DotNetNuke Forums")]
+[assembly: AssemblyCopyright("DotNetNuke® is copyright 2002-2011 by DotNetNuke Corporation. All Rights Reserved.")]
+[assembly: AssemblyTrademark("DotNetNuke")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2aea765-92c6-4bd8-8341-b4e14f0e4055")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
