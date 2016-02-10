5b1d8f9f0422c99e989a200ab3c66493d55d2603
Juvander <ilpo.juvander@hotmail.com>
2016-02-10 23:03:45 +0200

    Fix for timezone issue

--- branches/DotNetNuke.Forum.Library/UrlUtilityClass.cs
+++ branches/DotNetNuke.Forum.Library/UrlUtilityClass.cs
@@ -7,31 +7,23 @@ using System.Text.RegularExpressions;
 namespace DotNetNuke.Forum.Library
 {
     public class UrlUtilityClass
-    {        
+    {
         /// <summary>
-        /// Return shorter url
+        /// Return shorter url for linkName, if found
         /// </summary>
         /// <param name="url"></param>
         /// <returns></returns>
         public static string UrlShortener(string url)
         {
             string retval = url;
-            Match ret = Regex .Match (url, @">([^\s]*)<");
-            if (ret.Success  ==  true)
+            Match ret = Regex.Match(url, @">([^\s]*)<");
+            if (ret.Success == true)
             {
-                string linkName = ret.Groups [1].Value;
+                string linkName = ret.Groups[1].Value;
                 if (string.IsNullOrEmpty(linkName) == false && linkName.Length > 60)
                 {
-<<<<<<< HEAD
-                    retval = string.Format("{0}...{1}", linkName.Substring(0, 37), linkName.Substring(linkName.Length - 15, 15));
-                }
-                else
-                {
-                    retval = linkName;
-=======
                     string newLinkName = string.Format(">{0}...{1}<", linkName.Substring(0, 37), linkName.Substring(linkName.Length - 15, 15));
                     retval = url.Replace(ret.Groups[0].Value, newLinkName);
->>>>>>> 9f519d0... Fixed UrlShortener issue
                 }
             }
             return retval;
